93d8f486ffea507ef13c46e9acf278ebead6027f
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-03-10 10:29:55 +0300

    add load bg

---
+++ js/game.js
@@ -0,0 +1,20 @@
+const cvs = document.getElementById('canvas')
+const ctx = cvs.getContext('2d')
+
+const bird = new Image()
+const bg = new Image()
+const fg = new Image()
+const pipeUp = new Image()
+const pipeBottom = new Image()
+
+bird.src = 'img/bird.png'
+bg.src = 'img/bg.png'
+fg.src = 'img/fg.png'
+pipeUp.src = 'img/pipeUp.png'
+pipeBottom.src = 'img/pipeBottom.png'
+
+function draw() {
+  ctx.drawImage(bg, 0, 0)
+}
+
+pipeBottom.onload = draw
